da975e27d23da257c910bcdc2307f700e658d7e4
dyrhus <40840952+dyrhus@users.noreply.github.com>
2025-01-31 18:01:03 +0000

    script change

--- app/lib/demoScripts.ts
+++ app/lib/demoScripts.ts
@@ -4,6 +4,6 @@ Our program is fast, easy, and costs a fraction of what it would if you ran it t
 
 Let me show you how quick the onboarding process is. First, employees complete a short health questionnaire. Then, they schedule a video visit with one of our licensed healthcare professionals who will review their information and determine if they're eligible for the program.
 
-That's it! Once approved, eMed tracks adherence to ensure the program is effective and to avoid waste and abuse. Here's an example of the weekly check-in process: Employees simply log their weight, any side effects, and confirm they're taking their medication as prescribed. This helps us ensure the program is working effectively for each participant.`;
+That's it. Once approved, eMed tracks adherence to ensure the program is effective and to avoid waste and abuse. Here's an example of the weekly check-in process: Employees simply log their weight, any side effects, and confirm they're taking their medication as prescribed. This helps us ensure the program is working effectively for each participant.`;
 
 export const QA_PERMISSION_SCRIPT = "Next, you will be prompted to accept microphone permissions so that we can have a live conversation and answer any questions you may have.";
